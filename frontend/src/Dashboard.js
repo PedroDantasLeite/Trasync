8fae67838fec7a26026ab0ddf0a6e4fb2bbb32d0
joaoguilherme2003 <jggfm@cesar.school>
2023-06-12 11:51:51 -0300

    header (#3)

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -1,11 +1,26 @@
 import "./Dashboard.css";
-
+import bell from "./assets/bell.png";
+import juliana from "./assets/juliana.png";
 function Dashboard() {
     return (
         <>
-        <div>
-            <t1>dashboard</t1>
-        </div>
+        <header>
+            <div className="title">
+                <t1>Dashboard</t1>
+            </div>
+            <div className="perfil">
+                <img src={bell}></img>
+                <img src={juliana}></img>
+                <div>
+                    <p className="name">Juliana Silva</p>
+                    <p className="email">juliana@gmail.com</p>
+                </div>
+                
+            </div>
+            
+            
+        </header>
+        <body></body>
         </>
     );
 }
